c824736dd9c83b658d0d880fa2329b7f11167094
Noah S <72777308+nspahn@users.noreply.github.com>
2021-01-25 17:28:05 -0500

    Create empty.py

---
+++ empty.py
@@ -0,0 +1,18 @@
+#=====================================
+#Defining functions here
+#=====================================
+
+def add(a, b):
+  return a + b
+def sub(a,b):
+  return a - b
+
+
+#=====================================
+#Defining functions here
+#=====================================
+
+def test_add(a, b):
+  add(a, b)
+def test_add(a, b):
+  sub(a, b)
